1bf847dfcabe1741e99cc05ca5832ebdb483a4a3
SantiagoZ13 <diazsantiagoponce1@gmail.com>
2024-02-22 16:16:11 -0500

    explain callback hell

--- clase2-asincronismo.js
+++ clase2-asincronismo.js
@@ -11,3 +11,13 @@ let make = cook
 cook("Arroz", console.log, 10)
 cook("Huevo", console.log, 2)
 make("Mesa", console.log, 1)
+
+//Calbackhell. para que se ejecute una solo cuando se ejecuto otra
+cook("Arroz", function(ingredient){
+    console.log(ingredient)
+    cook("Huevo", function(ingredient){
+        console.log(ingredient)
+        make("Mesa", console.log, 1)
+    }, 2)
+}, 10)
+
